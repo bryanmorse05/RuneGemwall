aff447ba526e05cb0684ca28df42232ee01b0b3c
Bryan <bryanmorse05@gmail.com>
2018-09-18 22:32:19 -0700

    Got core stats up and running.  Need to remove methods from the other fragments so it's no longer crashing.

--- app/src/main/java/com/example/bryan/runegemwall/CharacterStats_Activity.java
+++ app/src/main/java/com/example/bryan/runegemwall/CharacterStats_Activity.java
@@ -1,6 +1,8 @@
 package com.example.bryan.runegemwall;
 
-import android.app.Fragment;
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -17,14 +19,14 @@ public class CharacterStats_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_stats_);
 
-        statsFrameLayout = findViewById(R.id.statsFrameLayout);
-        tabLayout = findViewById(R.id.tab_layout);
+        statsFrameLayout = findViewById(R.id.statsLayout);
+        tabLayout = findViewById(R.id.statsTabLayout);
 
         //Code that will display the character core stats first (the first tab)
         Fragment startingFragment = new CoreStats_Fragment();
         FragmentManager sfm = getSupportFragmentManager();
         FragmentTransaction sft = sfm.beginTransaction();
-        sft.replace(R.id.statsFrameLayout, startingFragment);
+        sft.replace(R.id.statsLayout, startingFragment);
         sft.commit();
 
         //perform setOnTabSelectedlistener even on TabLayout
@@ -37,22 +39,22 @@ public class CharacterStats_Activity extends AppCompatActivity {
                 Fragment fragment = null;
                 switch (tab.getPosition()) {
                     case 0:
-                        fragment = new CharacterStatsFragment();
+                        fragment = new CoreStats_Fragment();
                         break;
                     case 1:
-                        fragment = new SavingThrowsFragment();
+                        fragment = new Abilities_Fragment();
                         break;
                     case 2:
-                        fragment = new ProficiencyFragment();
+                        fragment = new SkillsTalents_Fragment();
                         break;
                     case 3:
-                        fragment = new EquipmentFragment();
+                        fragment = new Equipment_Fragment();
                         break;
                 }
 
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.statsFrameLayout, fragment);
+                ft.replace(R.id.statsLayout, fragment);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
                 ft.commit();
             }

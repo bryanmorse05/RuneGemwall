8bb47202064c0f205d696831128dafa818d071f3
bryanmorse <bryanmorse05@gmail.com>
2018-09-21 14:04:39 -0700

    Stats TabLayout complete, now need to fill in the fragments

--- app/src/main/java/com/example/bryan/runegemwall/activities/CharacterStats_Activity.java
+++ app/src/main/java/com/example/bryan/runegemwall/activities/CharacterStats_Activity.java
@@ -1,6 +1,8 @@
 package com.example.bryan.runegemwall.activities;
 
 //import android.app.Fragment;
+import android.app.Activity;
+import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.TabLayout;
@@ -8,7 +10,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.bryan.runegemwall.fragments.CoreStats_Fragment;
 import com.example.bryan.runegemwall.fragments.Equipment_Fragment;
@@ -19,16 +25,66 @@ import com.example.bryan.runegemwall.fragments.Skills_Fragment;
 
 public class CharacterStats_Activity extends AppCompatActivity {
 
+    ConstraintLayout statsConstraintLayout;
     FrameLayout statsLayout;
     TabLayout tabLayout;
+    EditText currentHitPoints, currentArmorClass, currentCharacterSpeed;
+    TextView hitPointsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_stats_);
 
+        statsConstraintLayout = findViewById(R.id.statsConstraintLayout);
         statsLayout = findViewById(R.id.statsLayout);
         tabLayout = findViewById(R.id.statsTabLayout);
+        currentHitPoints = findViewById(R.id.currentHitPoints);
+        currentArmorClass = findViewById(R.id.currentArmorClass);
+        currentCharacterSpeed = findViewById(R.id.currentCharacterSpeed);
+        hitPointsText = findViewById(R.id.hitPointsText);
+
+        //When the fields are touched is when the cursor appears.  It appears always on by default
+        currentHitPoints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentHitPoints.setCursorVisible(true);
+            }
+        });
+
+        currentArmorClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentArmorClass.setCursorVisible(true);
+            }
+        });
+
+        currentCharacterSpeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentCharacterSpeed.setCursorVisible(true);
+            }
+        });
+
+        //Call this whenever the user touches outside of the keyboard and the EditText fields,
+        //hiding the keyboard and making the cursors invisible again
+        statsConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentHitPoints.setCursorVisible(false);
+                currentArmorClass.setCursorVisible(false);
+                currentCharacterSpeed.setCursorVisible(false);
+
+                //I only need this so the focus is on something else other than the editText fields
+                hitPointsText.setFocusableInTouchMode(true);
+                hitPointsText.requestFocus();
+
+                hideKeyboardFrom(getApplicationContext(), view);
+
+            }
+        });
+
+
 
         //Code that will display the character core stats first (the first tab)
         Fragment startingFragment = new CoreStats_Fragment();
@@ -81,6 +137,12 @@ public class CharacterStats_Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+    //Cool little function I found online that works with fragments.  Can touch anywhere.
+    public static void hideKeyboardFrom(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
